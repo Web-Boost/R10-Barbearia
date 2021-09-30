d2e83eb1dc55d955eb20e25749f9b7a8541238ed
TorresLino <arthur.torres.de.lino@gmail.com>
2021-09-30 15:55:09 -0300

    funcionaro

--- teste_r10/agendamento/views.py
+++ teste_r10/agendamento/views.py
@@ -1,4 +1,5 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    render(request, 'index.html')
+    return render(request, '../templates/index.html')
